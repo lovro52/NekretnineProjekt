b38db6539789cd67b336c7b41e8aa72d339b7c54
Lovro Luka Matan <lmatan@student.unipu.hr>
2023-06-12 02:54:32 +0200

    UserQualityOfLife

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,47 @@
 <template>
   <v-app>
     <v-app-bar app>
-      <v-toolbar-title>Nekretnine
-      </v-toolbar-title>
+      <v-toolbar-title>Nekretnine </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn text to="/">Home</v-btn>
       <v-btn v-if="!currentUser" text to="/login">Login</v-btn>
-    <v-btn v-if="!currentUser" text to="/register">Register</v-btn>
+      <v-btn v-if="!currentUser" text to="/register">Register</v-btn>
       <v-btn text to="/profile">Profile</v-btn>
       <v-btn text to="/Nekretnine">Nekretnine</v-btn>
       <v-btn text to="/Test">Test</v-btn>
-
+      <v-menu bottom min-width="200px" rounded offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn icon x-large v-on="on">
+            <v-avatar color="brown" size="48">
+              <span class="white--text text-h5">{{ user.initials }}</span>
+            </v-avatar>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-list-item-content class="justify-center">
+            <div class="mx-auto text-center">
+              <v-avatar color="brown">
+                <span class="white--text text-h5">{{ user.initials }}</span>
+              </v-avatar>
+              <h3>{{ user.fullName }}</h3>
+              <p class="text-caption mt-1">
+                {{ user.email }}
+              </p>
+              <v-divider class="my-3"></v-divider>
+              <v-btn depressed rounded text> Edit Account </v-btn>
+              <v-divider class="my-3"></v-divider>
+              <v-btn depressed rounded text> Disconnect </v-btn>
+            </div>
+          </v-list-item-content>
+        </v-card>
+      </v-menu>
     </v-app-bar>
 
     <v-main>
       <router-view></router-view>
       <div v-if="currentUser">
-      <p>Logged in as: {{ currentUser.email }}</p>
-    </div>
+        <p>Logged in as: {{ currentUser.email }}</p>
+      </div>
     </v-main>
     <v-footer color="primary lighten-1" padless>
       <v-row justify="center" no-gutters>
@@ -30,30 +54,26 @@
 </template>
 
 <script>
-import {
-    auth,
-    onAuthStateChanged,
-  } from "@/firebase";
+import { auth, onAuthStateChanged } from "@/firebase";
 export default {
   data: () => ({
-    links: [
-      'Home',
-      'About Us',
-      'Contact Us',
-    ],
+    user: {
+      initials: "JD",
+      fullName: "John Doe",
+      email: "john.doe@doe.com",
+    },
+    links: ["Home", "About Us", "Contact Us"],
     currentUser: null,
   }),
 
   beforeCreate() {
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      this.currentUser = user; // Set the currently logged-in user
-    } else {
-      this.currentUser = null; // User is signed out
-    }
-  });
-},
-
-}
-
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.currentUser = user; // Set the currently logged-in user
+      } else {
+        this.currentUser = null; // User is signed out
+      }
+    });
+  },
+};
 </script>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,109 +1,146 @@
 <template>
-    <v-container fill-height fluid class="background">
-      <v-row align="center" justify="center">
-        <v-col align="center" justify="center" cols="12">
-          <v-card class="card-border" width="600px" outlined>
-            <v-card-title align="left">PROFILE</v-card-title>
-            <v-card-subtitle align="left">
-              Here you can change your password or update your info:
-            </v-card-subtitle>
-            <v-card-text class="card-text-border">
-              <v-form v-model="valid">
-                <v-text-field v-model="firstName" dense label="Name" clearble type="text" outlined></v-text-field>
-                <v-text-field v-model="lastName" dense label="Last Name" clearble type="text" outlined></v-text-field>
-                <v-text-field v-model="email" dense label="Email" clearble type="text" outlined></v-text-field>
-                <v-text-field v-model="newPassword" dense label="Password" clearble :append-icon="showIcon ? 'mdi-eye' : 'mdi-eye-off'
-                  " :rules="[rules.required, rules.min]" :type="showIcon ? 'text' : 'password'" outlined></v-text-field>
-              </v-form>
-            </v-card-text>
-            <v-card-actions class="card-actions">
-              <v-btn :disabled="isChangePassButtonDisabled ||
-                newPassword == null ||
-                !valid
-                " outlined @click="changePassword">
-                CHANGE PASSWORD
-              </v-btn>
-              <v-btn :disabled="isOkButtonDisabled || isUserDataSame" :loading="!hasDataArrived" outlined>
-                OK
-              </v-btn>
-              <v-btn @click="logout" outlined>
-                SignOut
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
-  
-  <script>
-  import {
-    doc,
-    db,
-    getDoc,
-    auth,
-    onAuthStateChanged,
-    updatePassword,
-    signOut,
-  } from "../firebase.js";
-  
-  export default {
-    name: "ProfileView",
-    components: {},
-    watch: {
-      newPassword: function (hasValue) {
-        if (hasValue) {
-          this.isChangePassButtonDisabled = false;
-        } else {
-          this.isChangePassButtonDisabled = true;
-        }
-      },
-      firstName: {
-        handler: function (newValue) {
-          this.compareFormData(newValue);
-        },
-        deep: true,
-      },
-      lastName: {
-        handler: function (newValue) {
-          this.compareFormData(newValue);
-        },
-        deep: true,
-      },
-      email: {
-        handler: function (newValue) {
-          this.compareFormData(newValue);
-        },
-        deep: true,
-      },
+  <v-container fill-height fluid class="background">
+    <v-avatar color="primary" size="56">
+      <v-icon dark> mdi-account-circle </v-icon>
+    </v-avatar>
+    <v-row align="center" justify="center">
+      <v-col align="center" justify="center" cols="12">
+        <v-card class="card-border" width="600px" outlined>
+          <v-card-title align="left">PROFILE</v-card-title>
+          <v-card-subtitle align="left">
+            Here you can change your password or update your info:
+          </v-card-subtitle>
+          <v-card-text class="card-text-border">
+            <v-form v-model="valid">
+              <v-text-field
+                v-model="firstName"
+                dense
+                label="Name"
+                clearble
+                type="text"
+                outlined
+              ></v-text-field>
+              <v-text-field
+                v-model="lastName"
+                dense
+                label="Last Name"
+                clearble
+                type="text"
+                outlined
+              ></v-text-field>
+              <v-text-field
+                v-model="email"
+                dense
+                label="Email"
+                clearble
+                type="text"
+                outlined
+              ></v-text-field>
+              <v-text-field
+                v-model="newPassword"
+                dense
+                label="Password"
+                clearble
+                :append-icon="showIcon ? 'mdi-eye' : 'mdi-eye-off'"
+                :rules="[rules.required, rules.min]"
+                :type="showIcon ? 'text' : 'password'"
+                outlined
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions class="card-actions">
+            <v-btn
+              :disabled="
+                isChangePassButtonDisabled || newPassword == null || !valid
+              "
+              outlined
+              @click="changePassword"
+            >
+              CHANGE PASSWORD
+            </v-btn>
+            <v-btn
+              :disabled="isOkButtonDisabled || isUserDataSame"
+              :loading="!hasDataArrived"
+              outlined
+            >
+              OK
+            </v-btn>
+            <v-btn @click="logout" outlined> SignOut </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import {
+  doc,
+  db,
+  getDoc,
+  auth,
+  onAuthStateChanged,
+  updatePassword,
+  signOut,
+} from "../firebase.js";
+
+export default {
+  name: "ProfileView",
+  components: {},
+  watch: {
+    newPassword: function (hasValue) {
+      if (hasValue) {
+        this.isChangePassButtonDisabled = false;
+      } else {
+        this.isChangePassButtonDisabled = true;
+      }
     },
-    data() {
-      return {
-        valid: null,
-        user: null,
-        firstName: null,
-        lastName: null,
-        email: null,
-        newPassword: null,
-        compareData: null,
-        userData: null,
-        hasDataArrived: false,
-        isOkButtonDisabled: false,
-        isUserDataSame: true,
-        isChangePassButtonDisabled: true,
-        showIcon: false,
-        rules: {
-          required: (value) => !!value || "Required.",
-          min: (v) => v?.length >= 6 || "Min 6 characters",
-          email: (v) =>
-            !v ||
-            /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
-            "E-mail must be valid",
-        },
-      };
+    firstName: {
+      handler: function (newValue) {
+        this.compareFormData(newValue);
+      },
+      deep: true,
     },
-    methods: {
-      logout() {
+    lastName: {
+      handler: function (newValue) {
+        this.compareFormData(newValue);
+      },
+      deep: true,
+    },
+    email: {
+      handler: function (newValue) {
+        this.compareFormData(newValue);
+      },
+      deep: true,
+    },
+  },
+  data() {
+    return {
+      valid: null,
+      user: null,
+      firstName: null,
+      lastName: null,
+      email: null,
+      newPassword: null,
+      compareData: null,
+      userData: null,
+      hasDataArrived: false,
+      isOkButtonDisabled: false,
+      isUserDataSame: true,
+      isChangePassButtonDisabled: true,
+      showIcon: false,
+      rules: {
+        required: (value) => !!value || "Required.",
+        min: (v) => v?.length >= 6 || "Min 6 characters",
+        email: (v) =>
+          !v ||
+          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+          "E-mail must be valid",
+      },
+    };
+  },
+  methods: {
+    logout() {
       signOut(auth)
         .then(() => {
           // User signed out successfully
@@ -115,69 +152,69 @@
           console.log("Logout error", error);
         });
     },
-      compareFormData() {
-        if (
-          this.userData.FirstName == this.firstName &&
-          this.userData.LastName == this.lastName &&
-          this.userData.Email == this.email
-        ) {
-          this.isUserDataSame = true;
-        } else {
-          this.isUserDataSame = false;
-        }
-      },
-      async getUserData(email) {
-        const docRef = doc(db, "users", email);
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-          let data = docSnap.data();
-          this.userData = data;
-          this.firstName = this.userData.FirstName;
-          this.lastName = this.userData.LastName;
-          this.email = this.userData.Email;
-  
-          this.hasDataArrived = true;
-        } else {
-          this.hasDataArrived = false;
-        }
-      },
-      changePassword() {
-        if (this.newPassword == null || this.valid != true) {
-          return;
-        }
-        this.isChangePassButtonDisabled = true;
-        updatePassword(this.user, this.userData.newPassword)
-          .then(() => {
-            this.isChangePassButtonDisabled = false;
-            console.log("Password updated successfully");
-          })
-          .catch((error) => {
-            this.isChangePassButtonDisabled = false;
-            //TODO: handle auth/requires-recent-login
-            console.log("Password update error ", error);
-          });
-      },
+    compareFormData() {
+      if (
+        this.userData.FirstName == this.firstName &&
+        this.userData.LastName == this.lastName &&
+        this.userData.Email == this.email
+      ) {
+        this.isUserDataSame = true;
+      } else {
+        this.isUserDataSame = false;
+      }
     },
-    beforeCreate() {
-      onAuthStateChanged(auth, (user) => {
-        if (user) {
-          this.user = user;
-          // User is signed in, see docs for a list of available properties
-          // https://firebase.google.com/docs/reference/js/firebase.User
-          const uid = user.uid;
-          this.getUserData(user.email);
-        } else {
-          // User is signed out
-          // ...
-        }
-      });
+    async getUserData(email) {
+      const docRef = doc(db, "users", email);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        let data = docSnap.data();
+        this.userData = data;
+        this.firstName = this.userData.FirstName;
+        this.lastName = this.userData.LastName;
+        this.email = this.userData.Email;
+
+        this.hasDataArrived = true;
+      } else {
+        this.hasDataArrived = false;
+      }
     },
-    created() {
-      console.log(this.userDataArrived);
+    changePassword() {
+      if (this.newPassword == null || this.valid != true) {
+        return;
+      }
+      this.isChangePassButtonDisabled = true;
+      updatePassword(this.user, this.userData.newPassword)
+        .then(() => {
+          this.isChangePassButtonDisabled = false;
+          console.log("Password updated successfully");
+        })
+        .catch((error) => {
+          this.isChangePassButtonDisabled = false;
+          //TODO: handle auth/requires-recent-login
+          console.log("Password update error ", error);
+        });
     },
-    mounted() {
-      console.log(this.userDataArrived);
-    },
-    destroyed() { },
-  };
-  </script>
+  },
+  beforeCreate() {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.user = user;
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        this.getUserData(user.email);
+      } else {
+        // User is signed out
+        // ...
+      }
+    });
+  },
+  created() {
+    console.log(this.userDataArrived);
+  },
+  mounted() {
+    console.log(this.userDataArrived);
+  },
+  destroyed() {},
+};
+</script>
