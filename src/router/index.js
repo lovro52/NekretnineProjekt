12c838b2a89e58626da1cdbe285035378a1caacc
Lovro Luka Matan <lmatan@student.unipu.hr>
2023-06-14 19:36:38 +0200

    NovaNekretninaSite

--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,56 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import LoginView from '../views/LoginView.vue'
-import Home from '../views/HomeView.vue'
-import RegisterView from '../views/RegisterView.vue'
-import ProfileView from '../views/ProfileView.vue'
-import NekretnineView from '../views/NekretnineView.vue'
-import DetaljiNekretnineView from "../views/DetaljiNekretnineView.vue"
-
-Vue.use(VueRouter)
+import Vue from "vue";
+import VueRouter from "vue-router";
+import LoginView from "../views/LoginView.vue";
+import Home from "../views/HomeView.vue";
+import RegisterView from "../views/RegisterView.vue";
+import ProfileView from "../views/ProfileView.vue";
+import NekretnineView from "../views/NekretnineView.vue";
+import DetaljiNekretnineView from "../views/DetaljiNekretnineView.vue";
+import NovaNekretninaView from "../views/NovaNekretninaView.vue";
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: Home
+    path: "/",
+    name: "home",
+    component: Home,
   },
   {
-    path: '/login',
-    name: 'login-view',
-    component: LoginView
+    path: "/login",
+    name: "login-view",
+    component: LoginView,
   },
   {
-    path: '/register',
-    name: 'register-view',
-    component: RegisterView
+    path: "/register",
+    name: "register-view",
+    component: RegisterView,
   },
   {
-    path: '/profile',
-    name: 'profile-view',
-    component: ProfileView
+    path: "/profile",
+    name: "profile-view",
+    component: ProfileView,
   },
   {
-    path: '/nekretnine',
-    name: 'nekretnine-view',
-    component: NekretnineView
+    path: "/nekretnine",
+    name: "nekretnine-view",
+    component: NekretnineView,
   },
   {
-    path: '/recepti/:recept_id',
-    name: 'detaljinekretnine-view',
-    component: DetaljiNekretnineView
+    path: "/recepti/:recept_id",
+    name: "detaljinekretnine-view",
+    component: DetaljiNekretnineView,
   },
-]
+  {
+    path: "/dodaj",
+    name: "dodajnekretninu",
+    component: NovaNekretninaView,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
