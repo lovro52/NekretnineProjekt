984f17ae130f44ac8224c666e8d4bec75cfadc2c
Lovro Luka Matan <lmatan@student.unipu.hr>
2023-08-13 11:24:29 +0200

    firebase update

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,26 @@
-import firebase from 'firebase/compat/app'
-import { doc, addDoc, getDoc, setDoc, getDocs, getCount, getFirestore, collection } from 'firebase/firestore/lite'
-import { getAuth, updatePassword, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail, confirmPasswordReset, beforeAuthStateChanged, onAuthStateChanged } from 'firebase/auth'
-import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+import firebase from "firebase/compat/app";
+import {
+  doc,
+  addDoc,
+  getDoc,
+  setDoc,
+  getDocs,
+  getCount,
+  getFirestore,
+  collection,
+} from "firebase/firestore/lite";
+import {
+  getAuth,
+  updatePassword,
+  signOut,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  confirmPasswordReset,
+  beforeAuthStateChanged,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -11,12 +30,14 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyB7dkHe-jksndinKmhDaACxEjXW2zugRaw",
-    authDomain: "nekretnine-ebad8.firebaseapp.com",
-    projectId: "nekretnine-ebad8",
-    storageBucket: "nekretnine-ebad8.appspot.com",
-    messagingSenderId: "191923220348",
-    appId: "1:191923220348:web:4e1a0ce2e61d8570d77ef9"
+  apiKey: "AIzaSyB7dkHe-jksndinKmhDaACxEjXW2zugRaw",
+  authDomain: "nekretnine-ebad8.firebaseapp.com",
+  databaseURL:
+    "https://nekretnine-ebad8-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "nekretnine-ebad8",
+  storageBucket: "nekretnine-ebad8.appspot.com",
+  messagingSenderId: "191923220348",
+  appId: "1:191923220348:web:4e1a0ce2e61d8570d77ef9",
 };
 
 // Initialize Firebase
@@ -26,20 +47,22 @@ const storage = getStorage(app);
 const db = getFirestore(app);
 
 export {
-    app, auth, storage,
-    getAuth,
-    db,
-    doc,
-    getDoc,
-    setDoc,
-    addDoc,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    sendPasswordResetEmail,
-    onAuthStateChanged,
-    ref,
-    uploadBytes,
-    getDownloadURL,
-    signOut,
-    updatePassword
-}
+  app,
+  auth,
+  storage,
+  getAuth,
+  db,
+  doc,
+  getDoc,
+  setDoc,
+  addDoc,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+  onAuthStateChanged,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  signOut,
+  updatePassword,
+};
